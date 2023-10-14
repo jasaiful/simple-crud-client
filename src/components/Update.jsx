557f6628d0ad772750efcc34f8244bd550e222b4
jasaiful <jasaiful@gmail.com>
2023-10-14 21:23:55 +0600

    Simple CRUD Client Site

---
+++ src/components/Update.jsx
@@ -0,0 +1,44 @@
+import { useLoaderData } from "react-router-dom";
+
+
+const Update = () => {
+
+    const loadedUsers = useLoaderData();
+
+    const handleUpdate = event => {
+        event.preventDefault();
+        const form = event.target;
+        const name = form.name.value;
+        const email = form.email.value;
+        console.log(name, email);
+        const updatedUser = { name, email };
+
+        // send updated to server side
+        fetch(`http://localhost:5000/users/${loadedUsers._id}`, {
+            method: 'PUT',
+            headers: {
+                'content-type': 'application/json'
+            },
+            body: JSON.stringify(updatedUser)
+        })
+            .then(res => res.json())
+            .then(data => {
+                console.log(data)
+            })
+    }
+
+    return (
+        <div>
+            <h3>Update Information of {loadedUsers.name} </h3>
+            <form onSubmit={handleUpdate}>
+                <input type="text" name="name" defaultValue={loadedUsers?.name} id="" />
+                <br />
+                <input type="email" name="email" defaultValue={loadedUsers?.email} id="" />
+                <br />
+                <input type="submit" value="Update" />
+            </form>
+        </div>
+    );
+};
+
+export default Update;
